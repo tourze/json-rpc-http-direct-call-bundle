c75e2b9c8f192f6c248ee00b7c195bf84ee66523
github-ci <you@example.com>
2025-04-27 16:52:26 +0000

    忽略无用调用堆栈

--- src/JsonRPCHttpDirectCallBundle.php
+++ src/JsonRPCHttpDirectCallBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\JsonRPCHttpDirectCallBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BacktraceHelper\Backtrace;
+use Tourze\JsonRPCHttpDirectCallBundle\Controller\JsonRpcController;
 
 class JsonRPCHttpDirectCallBundle extends Bundle
 {
+    public function boot(): void
+    {
+        parent::boot();
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcController::class))->getFileName());
+    }
 }
